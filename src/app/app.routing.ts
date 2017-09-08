d2a3a3f2f34c2943e8e9cd05852efd423aa27a41
Igor Alvarenga <alvarenga.igor@hotmail.com>
2017-09-08 15:06:23 -0300

    Curso detalhes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
@@ -8,7 +9,8 @@ import { ModuleWithProviders } from '@angular/core';
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'cursos', component: CursosComponent}
+    { path: 'cursos', component: CursosComponent},
+    { path: 'curso/:id', component: CursoDetalheComponent}
 ];
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
